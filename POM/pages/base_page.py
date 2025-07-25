57f827d983c805acb75ac51b9b304c5a09044fa6
DuBaoThoiTiet <irishatecilantro04@gmail.com>
2025-07-25 09:33:37 +0700

    Refactor BasePage class to use consistent driver reference and remove unused imports

--- POM/pages/base_page.py
+++ POM/pages/base_page.py
@@ -1,19 +1,15 @@
-"""
-
-"""
-import os
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
 class BasePage:
-    def __init__(self, browser):
-        self.driver = browser
+    def __init__(self, driver):
+        self.driver = driver # Store the WebDriver instance for use in other methods
     
     # This method tries to find an element by using XPath
     # TODO: Fix the method to capture screenshot the element when it fails
     def get_element(self, locator):
         try:
-            element = self.browser.driver.find_element(By.XPATH, locator)
+            element = self.driver.find_element(By.XPATH, locator)
             return element
         except NoSuchElementException:
             print(f"No elements found for locator: {locator}")
