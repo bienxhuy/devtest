4478e0dc31a54372c8efb227086d90b8f7c1f2a0
DuBaoThoiTiet <irishatecilantro04@gmail.com>
2025-07-25 11:48:39 +0700

    Refactor BuildAppLocators class to correct XPath syntax and consolidate enroll course button locator

--- POM/locators/build_auto_locators.py
+++ POM/locators/build_auto_locators.py
@@ -1,23 +1,18 @@
-class BuildAppAuto:
+class BuildAppLocators:
     # Header
-    HOME_HEADER = "a[//text() = 'home']"
-    COURSES_HEADER = "a[//text() = 'courses']"
-    RESOURCES_HEADER = "a[//text() = 'resources']"
-    COMMUNITY_HEADER = "a[//text() = 'community']"
-    ABOUT_HEADER = "a[//text() = 'about']"
-    CONTACT_HEADER = "a[//text() = 'contact']"
+    HOME_HEADER = "//a[text() = 'home']"
+    COURSES_HEADER = "//a[text() = 'courses']"
+    RESOURCES_HEADER = "//a[text() = 'resources']"
+    COMMUNITY_HEADER = "//a[text() = 'community']"
+    ABOUT_HEADER = "//a[text() = 'about']"
+    CONTACT_HEADER = "//a[text() = 'contact']"
 
     # Hero
     START_LEARNING_BUTTON = "//button[@id = 'start-learning-button']"
     WATCH_DEMO_BUTTON = "//button[@id = 'watch-demo-button']"
 
     # Courses
-    ENROLL_COURSE_BUTTON1 = "//button[@id = 'enroll-button-1']" 
-    ENROLL_COURSE_BUTTON2 = "//button[@id = 'enroll-button-2']" 
-    ENROLL_COURSE_BUTTON3 = "//button[@id = 'enroll-button-3']" 
-    ENROLL_COURSE_BUTTON4 = "//button[@id = 'enroll-button-4']" 
-    ENROLL_COURSE_BUTTON5 = "//button[@id = 'enroll-button-5']" 
-    ENROLL_COURSE_BUTTON6 = "//button[@id = 'enroll-button-6']" 
+    ENROLL_COURSE_BUTTON = "//button[@id = 'enroll-button-{}']" 
 
     # Resourses
     RESOURCES_BLOCK_PDF = "//div[contains(text(), 'PDF') and contains(@class, 'bg-white')]"
