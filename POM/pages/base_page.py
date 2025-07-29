d9a530ed168ffaad7a9e65a07ae884b649516fcd
DuBaoThoiTiet <irishatecilantro04@gmail.com>
2025-07-29 21:24:55 +0700

    Refactor BasePage class to use logging instead of print statements

--- POM/pages/base_page.py
+++ POM/pages/base_page.py
@@ -1,12 +1,19 @@
 import os
+import logging
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from datetime import datetime
 
+from POM.pages.logger import get_logger 
+
+# Use module-specific logger
+logger = get_logger(__name__)
+
 SCREENSHOT_DIR = "../utils/screenshots"
 
-class BasePage:
+# BasePage class    
+class BasePage: 
     def __init__(self, driver):
         self.driver = driver
 
@@ -16,8 +23,10 @@ class BasePage:
     def safe_click(self, locator, description="element",
                fallback_message=None, timeout=10):
         if not locator:
-            print(fallback_message or
-                f"[safe_click] No locator for '{description}'")
+            message = (fallback_message or
+                    f"[safe_click] No locator for '{description}'")
+            logger.warning(message)
+            self.take_screenshot(description)
             return
 
         try:
@@ -25,14 +34,22 @@ class BasePage:
                 EC.element_to_be_clickable(locator)
             )
             element.click()
-            print(f"[safe_click] Clicked on '{description}'")
+            logger.info(f"[safe_click] Clicked on '{description}'")
+
         except TimeoutException:
-            print(f"[safe_click] Timeout: '{description}' not clickable")
-            self._take_screenshot(description)
-        except Exception as e:
-            print(f"[safe_click] Error clicking '{description}': {e}")
+            logger.error(
+                f"[safe_click] Timeout while waiting for "
+                f"'{description}' to be clickable"
+            )
             self._take_screenshot(description)
 
+        except Exception as e:
+            logger.error(
+                f"[safe_click] Error clicking on '{description}': {e}"
+            )
+            self._take_screenshot(description)
+
+            
     # This method takes a screenshot and saves it with a timestamp
     def take_screenshot(self, name = "error"):
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -40,6 +57,6 @@ class BasePage:
         filepath = os.path.join(SCREENSHOT_DIR, filename)
         try:
             self.driver.save_screenshot(filepath)
-            print(f"[safe_click] Screenshot saved to: {filepath}")
+            logger.info(f"[safe_click] Screenshot saved to: {filepath}")
         except Exception as e:
-            print(f"[safe_click] Failed to take screenshot: {e}")
+            logger.info(f"[safe_click] Failed to take screenshot: {e}")
