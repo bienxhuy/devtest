600fd3c09dd61bb2b589fefba74779ad970744c3
Bien Xuan Huy <bienxhuy@gmail.com>
2025-07-30 14:57:04 +0700

    Refactor logger module to create unique log file names with timestamps and worker IDs

--- logs/logger.py
+++ logs/logger.py
@@ -9,24 +9,27 @@ from dotenv import load_dotenv
 load_dotenv()
 # Get the log directory from environment variables or use a default
 LOG_DIR = os.getenv('LOG_DIR', 'logs/logs_data')
-os.makedirs(LOG_DIR, exist_ok=True)     # Ensure the log directory exists
-# Create a log file with a timestamp for this session
+os.makedirs(LOG_DIR, exist_ok=True)
+# Create a unique log file name based on the current timestamp and worker ID (xdist)
+WORKER_ID = os.getenv("PYTEST_XDIST_WORKER", "main")
 SESSION_TIMESTAMP = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-LOG_FILE = os.path.join(LOG_DIR, f"{SESSION_TIMESTAMP}.log")
+LOG_FILE = os.path.join(LOG_DIR, f"{SESSION_TIMESTAMP}_{WORKER_ID}.log")
 
 
+# Get a logger instance, creating it if it doesn't exist
+# This function caches the logger instances to
+# avoid creating multiple instances with the same name
 def get_logger(name=__name__):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
 
+    # Check if the logger already has handlers to avoid duplicate logs
     if not logger.handlers:
         formatter = logging.Formatter(
             '%(asctime)s - %(levelname)s - %(message)s'
         )
         file_handler = logging.FileHandler(LOG_FILE)
-        print(f"[logger.py] Logger module loaded. Timestamp = {SESSION_TIMESTAMP}")
-
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
-
+                
     return logger
