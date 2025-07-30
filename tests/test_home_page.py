4f888788c665c888b3fe4715c65a9414cf8ded95
Bien Xuan Huy <bienxhuy@gmail.com>
2025-07-30 17:29:31 +0700

    Seperate test logic into smaller test functions and reduce test cases

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -1,6 +1,10 @@
+"""_summary_
+This module contains tests for the HomePage class.
+"""
 import os
 import pytest
 from dotenv import load_dotenv
+from POM.notifications.home_page_notifications import HomePageNotifications as Noti
 from POM.pages.home_page import HomePage
 from logs.logger import get_logger
 
@@ -15,31 +19,37 @@ load_dotenv()
 BASE_URL = os.getenv('BASE_URL')
 
 
-# This is a test for the home page of the application
-# This test will ensure that the home page loads correctly,
-# and all components are clickable
+# This function tests the home page header click functionality
 @pytest.mark.smoke
-def test_all_components(driver):
+def test_click_header(driver):
     # Initialize the HomePage object
     page = HomePage(driver=driver)
     # Navigate to the base URL
     driver.get(BASE_URL)
+    # Click the home header and check the notification
+    notification = page.click_home_header()
+    assert notification == Noti.HEADER_CLICKED_SUCCESS_NOTIFICATION
+        
 
-    try:
-        # Click on every component in the home page
-        page.click_home_header()
-        page.click_about_header()
-        page.click_contact_header()
-        page.click_courses_header()
-        page.click_community_header()
-        page.click_resources_header()
-        page.click_start_learning()
-        page.click_watch_demo()
-        page.click_all_enroll_button()
-        page.click_pdf_resource_block()
-        page.click_video_resource_block()
-        page.click_codeex_resource_block()
-        page.click_resource_download_icon()
+# This function tests the Start Learning button click functionality
+@pytest.mark.smoke
+def test_click_start_learning_button(driver):
+    # Initialize the HomePage object
+    page = HomePage(driver=driver)
+    # Navigate to the base URL
+    driver.get(BASE_URL)
+    # Click the Start Learning button and check the notification
+    notification = page.click_start_learning()
+    assert notification == Noti.START_LEARNING_BUTTON_CLICKED_SUCCESS_NOTIFICATION
 
-    except Exception as e:
-        logger.error(f"An error occurred while clicking components: {e}")
+
+# This function tests the Enroll Course button click functionality
+@pytest.mark.smoke
+def test_click_enroll_button(driver):
+    # Initialize the HomePage object
+    page = HomePage(driver=driver)
+    # Navigate to the base URL
+    driver.get(BASE_URL)
+    # Click the Enroll Course button and check the notification
+    notification = page.click_enroll_button()
+    assert notification == Noti.ENROLL_COURSE_BUTTON_CLICKED_SUCCESS_NOTIFICATION
