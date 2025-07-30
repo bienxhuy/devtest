3453245be58bcd5498de297d78175694409e8217
Bien Xuan Huy <bienxhuy@gmail.com>
2025-07-30 11:16:11 +0700

    Refactor conftest.py to include sys.path modification for module imports

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,13 @@
 import pytest
-from dotenv import load_dotenv
+import sys
+import os
 from selenium import webdriver
+
+
+# Go up one directory to include dev_test in sys.path
+# This allows the tests (files) to import modules from the dev_test package
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 
 # This is a pytest fixture that initializes a browser instance for testing
 # Provides a browser instance to the tests
