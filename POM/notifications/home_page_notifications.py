dba6dfcde9e79aeeb566065cbac9a53282f46eb7
Bien Xuan Huy <bienxhuy@gmail.com>
2025-07-30 17:27:39 +0700

    Add HomePageNotifications class with success notification messages

---
+++ POM/notifications/home_page_notifications.py
@@ -0,0 +1,7 @@
+class HomePageNotifications:
+    # Header clicked notification
+    HEADER_CLICKED_SUCCESS_NOTIFICATION = "Header clicked successfully"
+    # Start learning button clicked notification
+    START_LEARNING_BUTTON_CLICKED_SUCCESS_NOTIFICATION = "Start Learning button clicked successfully"
+    # Enroll course button clicked notification
+    ENROLL_COURSE_BUTTON_CLICKED_SUCCESS_NOTIFICATION = "Enroll Course button clicked successfully"
