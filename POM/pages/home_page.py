90b47348b9f54958af270aa4706f2969902c38cc
Bien Xuan Huy <bienxhuy@gmail.com>
2025-07-30 17:28:55 +0700

    Refactor click methods in HomePage class to handle success notifications and improve readability

--- POM/pages/home_page.py
+++ POM/pages/home_page.py
@@ -1,3 +1,4 @@
+from POM.notifications.home_page_notifications import HomePageNotifications
 from POM.pages.base_page import BasePage
 from POM.locators.home_page_locators import HomePageLocators as el  
 
@@ -7,106 +8,36 @@ class HomePage(BasePage):
 
     # Header clicks 
     def click_home_header(self):
-        self.safe_click(
+        isSuccess = self.safe_click(
             el.HOME_HEADER,
             description="Home Header",
             fallback_message=(
                 "[click_home_header] Home header locator not found"
             )
         )
-
-    def click_courses_header(self):
-        self.safe_click(
-            el.COURSES_HEADER,
-            description="Courses Header",
-            fallback_message=(
-                "[click_courses_header] Courses header locator not found"
-            )
-        )
-
-    def click_resources_header(self):
-        self.safe_click(
-            el.RESOURCES_HEADER,
-            description="Resources Header",
-            fallback_message=(
-                "[click_resources_header] Resources header locator not found"
-            )
-        )
-
-    def click_community_header(self):
-        self.safe_click(
-            el.COMMUNITY_HEADER,
-            description="Community Header",
-            fallback_message=(
-                "[click_community_header] Community header locator not found"
-            )
-        )
-
-    def click_about_header(self):
-        self.safe_click(
-            el.ABOUT_HEADER,
-            description="About Header",
-            fallback_message=(
-                "[click_about_header] About header locator not found"
-            )
-        )
-
-    def click_contact_header(self):
-        self.safe_click(
-            el.CONTACT_HEADER,
-            description="Contact Header",
-            fallback_message=(
-                "[click_contact_header] Contact header locator not found"
-            )
-        )
+        if (isSuccess):
+            return HomePageNotifications.HEADER_CLICKED_SUCCESS_NOTIFICATION
 
     # Hero button clicks
     def click_start_learning(self):
-        self.safe_click(
+        isSuccess = self.safe_click(
             el.START_LEARNING_BUTTON,
-            description="Start Learning button"
+            description="Start Learning button",
+            fallback_message=(
+                "[click_start_learning] Start Learning button locator not found"
+            )
         )
-
-    def click_watch_demo(self):
-        self.safe_click(
-            el.WATCH_DEMO_BUTTON,
-            description="Watch Demo button"
-        )
+        if (isSuccess):
+            return HomePageNotifications.START_LEARNING_BUTTON_CLICKED_SUCCESS_NOTIFICATION
     
-    # Click Enroll Course buttons
-    def click_all_enroll_button(self):
-        buttons = self.driver.get_elements(el.ENROLL_COURSE_BUTTON)
-
-        for index, button in enumerate(buttons, start=1):
-            self.safe_click_element(
-                button,
-                description=f"Enroll Button #{index}"
-            )      
-
-    # Resource clicks       
-    def click_pdf_resource_block(self):
-        self.safe_click(
-            el.RESOURCES_BLOCK_PDF,
-            description="PDF resource block",
-            fallback_message="[click_pdf_resource_block] Locator not found"
+    # Click Enroll Course button
+    def click_enroll_button(self):
+        isSuccess = self.safe_click(
+            el.ENROLL_COURSE_BUTTON,
+            description="Enroll Selenium Button",
+            fallback_message=(
+                "[click_all_enroll_button] Enroll Selenium Button locator not found"
+            )
         )
-
-    def click_video_resource_block(self):
-        self.safe_click(
-            el.RESOURCES_BLOCK_VIDEO,
-            description="VIDEO resource block",
-            fallback_message="[click_video_resource_block] Locator not found"
-        )
-
-    def click_codeex_resource_block(self):
-        self.safe_click(
-            el.RESOURCES_BLOCK_CODEEX,
-            description="CODEEX resource block",
-            fallback_message="[click_codeex_resource_block] Locator not found"
-        )
-        
-    def click_resource_download_icon(self):
-        self.safe_click(
-            el.RESOURCES_DOWNLOAD_ICON,
-            description="Resource Download Icon"
-        )
+        if (isSuccess):
+            return HomePageNotifications.ENROLL_COURSE_BUTTON_CLICKED_SUCCESS_NOTIFICATION
