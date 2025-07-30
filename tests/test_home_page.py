7fd5d434e4be69050d6880d9e3658fdd773fb6ff
Bien Xuan Huy <bienxhuy@gmail.com>
2025-07-30 14:05:03 +0700

    Refactor test_home_page.py to initialize logger and implement component click tests

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -1,8 +1,12 @@
 import os
-import sys
 import pytest
 from dotenv import load_dotenv
 from POM.pages.home_page import HomePage
+from logs.logger import get_logger
+
+
+# Initialize the logger
+logger = get_logger(__name__)
 
 
 # Load environment variables from .env file
@@ -15,13 +19,27 @@ BASE_URL = os.getenv('BASE_URL')
 # This test will ensure that the home page loads correctly,
 # and all components are clickable
 @pytest.mark.smoke
-def test_build_auto_tw_page(driver):
+def test_all_components(driver):
     # Initialize the HomePage object
     page = HomePage(driver=driver)
     # Navigate to the base URL
     driver.get(BASE_URL)
 
-    # Click on every component in the page
-    # TODO: Implement the logic to click on each component
-    # Example:
-    # page.click_resource_download_icon()
+    try:
+        # Click on every component in the home page
+        page.click_home_header()
+        page.click_about_header()
+        page.click_contact_header()
+        page.click_courses_header()
+        page.click_community_header()
+        page.click_resources_header()
+        page.click_start_learning()
+        page.click_watch_demo()
+        page.click_all_enroll_button()
+        page.click_pdf_resource_block()
+        page.click_video_resource_block()
+        page.click_codeex_resource_block()
+        page.click_resource_download_icon()
+
+    except Exception as e:
+        logger.error(f"An error occurred while clicking components: {e}")
