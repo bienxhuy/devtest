ab620fcb6b565ba0654a38eb1b69ac0922931a09
Bien Xuan Huy <bienxhuy@gmail.com>
2025-07-31 14:53:33 +0700

    Remove adding syspath in conftest and treat test module as package

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,11 +4,6 @@ import os
 from selenium import webdriver
 
 
-# Go up one directory to include dev_test in sys.path
-# This allows the tests (files) to import modules from the dev_test package
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-
 # This is a pytest fixture that initializes a driver instance for testing
 # Provides a driver instance to the tests
 @pytest.fixture(scope="session")
