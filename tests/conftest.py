26a777d8d29e7e786688cce07c6f426f2adc7165
Bien Xuan Huy <bienxhuy@gmail.com>
2025-08-01 11:04:40 +0700

    Add maximize_window to driver to render website properly

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,5 +9,6 @@ def driver():
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
     driver = webdriver.Chrome(options=options)
+    driver.maximize_window()
     yield driver
     driver.quit()
