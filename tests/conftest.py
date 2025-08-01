05c458a80255add8dc04960019e4794f1dc0e16e
Bien Xuan Huy <bienxhuy@gmail.com>
2025-08-01 10:05:39 +0700

    Remove unnecessary window maximization in driver fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,5 @@ def driver():
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
     driver = webdriver.Chrome(options=options)
-    driver.maximize_window()
     yield driver
     driver.quit()
