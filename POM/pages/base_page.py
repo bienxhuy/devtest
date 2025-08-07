0f550f50d5fdb6bb92d014e722a08c1d7301c576
Bien Xuan Huy <bienxhuy@gmail.com>
2025-08-07 15:24:47 +0700

    Remove log and screenshot in base_page to simplify class

--- POM/pages/base_page.py
+++ POM/pages/base_page.py
@@ -1,21 +1,7 @@
-import os
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-from datetime import datetime
-from logs.logger import get_logger
-from dotenv import load_dotenv
-
-
-# Load environment variables from .env file
-load_dotenv()
-# Use module-specific logger
-logger = get_logger(__name__)
-# Directory to save screenshots
-# TODO: Create sub folder for screenshots of each test session
-SCREENSHOT_DIR = os.getenv('SCREENSHOT_DIR', 'utils/screenshots')
-os.makedirs(SCREENSHOT_DIR, exist_ok=True)
 
 
 # BasePage class    
@@ -26,31 +12,21 @@ class BasePage:
     # This method safely clicks an element, waiting for it to be clickable
     # TODO: Add ultility methods: safe_send_keys, safe_get_element_text
     # is_element_visible
-    def safe_click(self, locator, description="element",
-               fallback_message=None, timeout=10):
+    def safe_click(self, locator, success_message, element="element", timeout=10):
         if not locator:
-            message = (fallback_message or
-                    f"[safe_click] No locator for '{description}'")
-            logger.warning(message)
-            return
+            return f"[safe_click] No locator for '{element}'"
 
+        # Wait for the element to be clickable and click it
         try:
             element = WebDriverWait(self.driver, timeout).until(
-                EC.element_to_be_clickable((By.XPATH,locator))
+                EC.element_to_be_clickable((By.XPATH, locator))
             )
             element.click()
-            logger.info(f"[safe_click] Clicked on '{description}'")
-            return True
+            return success_message
 
         except TimeoutException:
-            logger.error(
-                f"[safe_click] Timeout while waiting for "
-                f"'{description}' to be clickable"
-            )
+            return (f"Timeout while waiting for "
+                    f"'{element}' to be clickable")
 
         except Exception as e:
-            logger.error(
-                f"[safe_click] Error clicking on '{description}': {e}"
-            )
-            
-        return False
+            return (f"Error clicking '{element}': {str(e)}")
