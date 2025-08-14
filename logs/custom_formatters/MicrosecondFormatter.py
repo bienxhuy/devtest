a932c501c77a43ad7c27e1962aad5e8b193643a5
Bien Xuan Huy <bienxhuy@gmail.com>
2025-08-15 04:36:24 +0700

    Remove MicrosecondFormatter

--- logs/custom_formatters/MicrosecondFormatter.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-
-class MicrosecondFormatter(logging.Formatter):
-    def format(self, record):
-        record.unix_micro_ts = int(record.created * 1000000)
-        return super().format(record)
