cb0310fcf72a9c1bbb69616ccf7b57fc408d5dc7
Bien Xuan Huy <bienxhuy@gmail.com>
2025-08-22 08:58:45 +0700

    change chrome driver options

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,9 @@ from models.TestContext import context
 def driver():
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
+    options.add_argument("--no-sandbox")  # Required for Docker
+    options.add_argument("--disable-dev-shm-usage")  # Avoids issues with shared memory in Docker
+    options.add_argument("--disable-gpu")  # Disable GPU in headless mode
     driver = webdriver.Chrome(options=options)
     driver.maximize_window()
     yield driver
