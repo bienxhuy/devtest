febd1f8eda35610ffc0f7bd35cd58c643b7796ca
Bien Xuan Huy <bienxhuy@gmail.com>
2025-09-05 14:36:39 +0700

    update headless mode for web  driver

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,7 @@ def driver():
     options = webdriver.ChromeOptions()
     
     # Add required arguments for running Chrome in Docker
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
