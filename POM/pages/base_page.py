e1061cff79c3d4b68ea35d704907eec6d8fad7e7
Bien Xuan Huy <bienxhuy@gmail.com>
2025-10-02 10:30:59 +0700

    remove logs in base page

--- POM/pages/base_page.py
+++ POM/pages/base_page.py
@@ -2,11 +2,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-from logs.logger import get_logger
-
-
-# Get a logger instance for logging within the page objects
-logger = get_logger()
 
 
 # BasePage class    
@@ -19,24 +14,19 @@ class BasePage:
     # is_element_visible
     def safe_click(self, locator, success_message, element="element", timeout=10):
         if not locator:
-            logger.error(f"[POM - BasePage] No locator for '{element}'")
             return f"No locator for '{element}'"
 
         # Wait for the element to be clickable and click it
         try:
-            logger.info(f"[POM - BasePage] Waiting for '{element}' to be clickable")
             element = WebDriverWait(self.driver, timeout).until(
                 EC.element_to_be_clickable((By.XPATH, locator))
             )
             element.click()
-            logger.info(f"[POM - BasePage] Clicked '{element}' successfully")
             return success_message
 
         except TimeoutException:
-            logger.error(f"[POM - BasePage] - Timeout while waiting for '{element}' to be clickable")
             return (f"Timeout while waiting for "
                     f"'{element}' to be clickable")
 
         except Exception as e:
-            logger.error(f"[POM - BasePage] Error clicking '{element}': {str(e)}")
             return (f"Error clicking '{element}'")
