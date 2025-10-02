748e31884e7dc94113c1125a385de8c63e70fe68
Bien Xuan Huy <bienxhuy@gmail.com>
2025-10-02 11:17:20 +0700

    fix: add logger for error handling in safe_click method

--- POM/pages/base_page.py
+++ POM/pages/base_page.py
@@ -2,6 +2,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from logs.logger import get_logger
+
+
+# Create a logger to log error
+logger = get_logger()
 
 
 # BasePage class    
@@ -10,23 +15,24 @@ class BasePage:
         self.driver = driver
 
     # This method safely clicks an element, waiting for it to be clickable
-    # TODO: Add ultility methods: safe_send_keys, safe_get_element_text
-    # is_element_visible
     def safe_click(self, locator, success_message, element="element", timeout=10):
         if not locator:
             return f"No locator for '{element}'"
 
         # Wait for the element to be clickable and click it
         try:
-            element = WebDriverWait(self.driver, timeout).until(
+            element_object = WebDriverWait(self.driver, timeout).until(
                 EC.element_to_be_clickable((By.XPATH, locator))
             )
-            element.click()
+            element_object.click()
             return success_message
 
         except TimeoutException:
+            logger.error(f"[BASE_PAGE] Timeout while " 
+                         f"waiting for {element} to be clickable")
             return (f"Timeout while waiting for "
                     f"'{element}' to be clickable")
 
         except Exception as e:
+            logger.error(f"[BASE_PAGE] Error clicking {element}: {e}")
             return (f"Error clicking '{element}'")
