434764c7de77ddc41a93b6aa8040e6d77f554d44
Bien Xuan Huy <bienxhuy@gmail.com>
2026-03-13 19:01:25 +0700

    refactor: update home page locators and remove unused notification messages

--- POM/locators/home_page_locators.py
+++ POM/locators/home_page_locators.py
@@ -1,7 +1,6 @@
 class HomePageLocators:
     # Header
-    HOME_HEADER = "//*[@id='root']/div/header/div/div/nav/a[1]"
-    # Button
-    START_LEARNING_BUTTON = "//button[@id = 'start-learning-button']"
-    # Course
-    ENROLL_COURSE_BUTTON = "//*[@id='enroll-button-0']"
+    HOME_HEADER = "//*[@id=\"home-greeting\"]"
+    # Log Button
+    HOME_ACTION_BUTTON = "//*[@id=\"home-action-button\"]"
+    
--- POM/pages/home_page.py
+++ POM/pages/home_page.py
@@ -1,32 +1,18 @@
 from POM.notifications.home_page_notifications import HomePageNotifications as Noti
 from POM.pages.base_page import BasePage
-from POM.locators.home_page_locators import HomePageLocators as el  
+from POM.locators.home_page_locators import HomePageLocators as el
 
 
 class HomePage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
 
-    # Header clicks 
-    def click_home_header(self):
-        return self.safe_click(
-            el.HOME_HEADER,
-            element="Home Header",
-            success_message=Noti.HEADER_CLICKED_SUCCESS,
-        )
+    # Click home action button
+    def click_home_action_button(self):
+        self.click(el.HOME_ACTION_BUTTON)
+        return Noti(self.driver)
 
-    # Hero button clicks
-    def click_start_learning(self):
-        return self.safe_click(
-            el.START_LEARNING_BUTTON,
-            element="Start Learning button",
-            success_message=Noti.START_BUTTON_CLICKED_SUCCESS,
-        )
-    
-    # Click Enroll Course button
-    def click_enroll_button(self):
-        return self.safe_click(
-            el.ENROLL_COURSE_BUTTON,
-            element="Enroll Selenium Button",
-            success_message=Noti.ENROLL_BUTTON_CLICKED_SUCCESS,
-        )
+    # Check home header
+    def check_home_header(self, expected_header):
+        actual_header = self.get_inner_text(el.HOME_HEADER)
+        return actual_header == expected_header
